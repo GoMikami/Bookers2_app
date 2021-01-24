3acf25837543f8516bd6a666764570b9b3cb9411
GoMikami <08g0531mkm@gmail.com>
2021-01-24 19:13:25 +0000

    Topic2

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '08ea5d263c9abba5d2d2b5e0319291ceccef77d28102a8fae2b46933edd8b509fcb62f53a9d236840c7c752b10e7d5bf5ae0f51fd00e87aa6238e4dcdf627839'
